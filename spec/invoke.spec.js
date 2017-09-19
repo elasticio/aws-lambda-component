094c8f38e576ebbf332118a0dd8b3278d4d68536
Renat Zubairov <renat.zubairov@gmail.com>
2017-09-19 16:36:07 +0200

    Updated tests

--- spec/invoke.spec.js
+++ spec/invoke.spec.js
@@ -5,32 +5,79 @@ const assert = require('assert');
 
 describe('Given invoke action', function () {
 
-    it('should execute a call', function () {
-        AWS.mock('Lambda', 'invoke', function (params, callback) {
-            assert.deepStrictEqual(params, {
-                FunctionName: 'foo',
-                InvocationType: 'RequestResponse',
-                LogType: 'None',
-                Payload: JSON.stringify({
+    describe('and lambda function returning string', () => {
+        beforeEach(() => {
+            AWS.mock('Lambda', 'invoke', function (params, callback) {
+                assert.deepStrictEqual(params, {
+                    FunctionName: 'foo',
+                    InvocationType: 'RequestResponse',
+                    LogType: 'None',
+                    Payload: JSON.stringify({
+                        start: 'the action'
+                    }),
+                    Qualifier: '$LATEST'
+                });
+                callback(null, {
+                    Payload: 'Hello world'
+                });
+            });
+        });
+
+        it('should execute a call with string response', function () {
+            return action.process({
+                body: {
                     start: 'the action'
-                }),
-                Qualifier: '$LATEST'
-            });
-            callback(null, {
-                Payload: 'Hello world'
-            });
-        });
-        return action.process({
-            body: {
-                start: 'the action'
-            }
-        }, {
-            functionName: 'foo'
-        }).then((result) => {
-            assert.ok(result);
-            assert.deepEqual(result.body, {
-                result: 'Hello world'
+                }
+            }, {
+                functionName: 'foo'
+            }).then((result) => {
+                assert.ok(result);
+                assert.deepEqual(result.body, {
+                    result: 'Hello world'
+                });
             });
         });
+
+        afterEach(() => AWS.restore());
     });
+
+    describe('and lambda function returning JSON', () => {
+        beforeEach(() => {
+            AWS.mock('Lambda', 'invoke', function (params, callback) {
+                assert.deepStrictEqual(params, {
+                    FunctionName: 'foo',
+                    InvocationType: 'RequestResponse',
+                    LogType: 'None',
+                    Payload: JSON.stringify({
+                        start: 'the action'
+                    }),
+                    Qualifier: '$LATEST'
+                });
+                callback(null, {
+                    Payload: JSON.stringify({
+                        message: 'Hello world!'
+                    })
+                });
+            });
+        });
+
+        it('should execute a call with JSON response', function () {
+            return action.process({
+                body: {
+                    start: 'the action'
+                }
+            }, {
+                functionName: 'foo'
+            }).then((result) => {
+                assert.ok(result);
+                console.log(result.body);
+                assert.deepEqual(result.body, {
+                    message: 'Hello world!'
+                });
+            });
+        });
+
+        afterEach(() => AWS.restore());
+    });
+
 });
